32981b6e8b7afb711d77a2a995cc237b2797321a
Esteban <estebandpisani@gmail.com>
2022-06-03 13:20:41 -0300

    Implementando props en Testimonio para dinamismo.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import Testimonio from './components/Testimonio.jsx';
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio
+        nombre='Emma Bostian'
+        pais='Sweden'
+        imagen=''
+        cargo='Software Engineer'
+        empresa='Spotify'
+        testimonio="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify." />
+      </div>
     </div>
   );
 }
