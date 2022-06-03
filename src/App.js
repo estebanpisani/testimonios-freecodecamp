fd201c6a09f1b266ea0e233106e8da65c23039a1
Esteban <estebandpisani@gmail.com>
2022-06-03 12:21:23 -0300

    Primer componente - Testimonio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Testimonio from './components/Testimonio.jsx';
 function App() {
   return (
     <div className="App">
-      
+      <Testimonio />
     </div>
   );
 }
